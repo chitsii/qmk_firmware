71d0a2a9716cb81dfd18a0ef735755615ef937c2
FanJiang <59207213+chitsii@users.noreply.github.com>
2025-11-14 02:39:20 +0900

    feat: Add speculative hold support and new NUM_SYM layer

---
+++ keyboards/keychron/q11/ansi_encoder/keymaps/myq11/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Tap-Hold設定の最適化
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY  // キーごとにTapping Termをカスタマイズ
+#define PERMISSIVE_HOLD       // より積極的にホールドと判定
--- keyboards/keychron/q11/ansi_encoder/keymaps/myq11/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/myq11/keymap.c
@@ -22,51 +22,65 @@ enum layers{
     WIN_BASE,
     WIN_BASE_JP,
     WIN_FN,
+    NUM_SYM,  // 記号レイヤー追加
 };
 
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
+// Space長押しでNUM_SYMレイヤーへ
+#define SPC_SYM LT(NUM_SYM, KC_SPC)
+#define ENT_CTL RCTL_T(KC_ENT)
+#define ENT_GUI RGUI_T(KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
-        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
+        KC_MUTE,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
+        _______,  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            DF(WIN_BASE),
         _______,  KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             DF(WIN_BASE_JP),
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LGUI,  KC_LALT,  KC_LCTL,  MO(MAC_FN),         KC_SPC,                        KC_ENT,             KC_CAPS,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LGUI,  KC_LALT,  KC_LCTL,  MO(MAC_FN),         SPC_SYM,                       ENT_GUI,            KC_CAPS,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_91_ansi(
         RM_TOGG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  RM_TOGG,
-        _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
+        _______,  _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,            _______,
         _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_PGUP,  LGUI(KC_LEFT), KC_UP, LGUI(KC_RGHT), _______,  _______,  _______,  _______,  _______,
         _______,  _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,              _______,            _______,
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  KC_NO,    _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
-        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
+        KC_MUTE,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
+        _______,  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            DF(WIN_BASE),
         _______,  KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             DF(WIN_BASE_JP),
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),         KC_SPC,                        KC_ENT,             KC_F13,   MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),         SPC_SYM,                       ENT_CTL,            JP_ZKHK,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_BASE_JP] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
-        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
+        KC_MUTE,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
+        _______,  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            DF(WIN_BASE),
         _______,  KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             DF(WIN_BASE_JP),
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),        KC_SPC,                        KC_ENT,             JP_ZKHK,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),        SPC_SYM,                        ENT_CTL,            JP_ZKHK,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_91_ansi(
         RM_TOGG,  _______,  KC_BRID,  KC_BRIU,  LGUI(KC_TAB), LGUI(KC_E), RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RM_TOGG,
-        _______,  _______,  _______,  _______,  _______,      _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        _______,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,        KC_F4,      KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     _______,            _______,
         _______,  KC_CAPS,  KC_NO,    KC_NO,    KC_NO,        KC_NO,      KC_NO,     KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   _______,  _______,    _______,  _______,            _______,
         _______,  _______,  KC_NO,    KC_NO,    KC_NO,        KC_NO,      KC_NO,     KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,              _______,            _______,
         _______,  _______,            _______,  _______,      _______,    _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,                _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
+
+    [NUM_SYM] = LAYOUT_91_ansi(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
+        _______,  KC_TILD,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,   _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_PLUS,  KC_MINS,  KC_EQL,  _______,  _______,    _______,  _______,            _______,
+        _______,  _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,   KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,              KC_PIPE,            _______,
+        _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,
+        _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
 };
 
@@ -192,6 +206,29 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [WIN_BASE_JP] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) }
+    [WIN_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [NUM_SYM]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif // ENCODER_MAP_ENABLE
+
+// Speculative Hold コールバック
+// Space/NUM_SYMレイヤーとEnter/Ctrlに対してSpeculative Holdを有効化
+bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case SPC_SYM:  // Space/NUM_SYM layer-tap
+        case ENT_CTL:  // Enter/Ctrl
+            return true;
+    }
+    return false;  // その他のmod-tapキーには適用しない
+}
+
+// Tapping Term の個別設定（オプション）
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SPC_SYM:
+        case ENT_CTL:
+            return 180;  // Space/Enterは少し短めに（デフォルト200ms）
+        default:
+            return TAPPING_TERM;
+    }
+}
