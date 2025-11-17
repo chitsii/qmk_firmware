35050084d56cbc288bc4ebe40a7c79cc2249d24a
FanJiang <59207213+chitsii@users.noreply.github.com>
2025-11-17 18:16:32 +0900

    fix: Update keymap for Keychron Q11 ANSI Encoder layout to correct function key assignments and enhance NUM_SYM layer

--- keyboards/keychron/q11/ansi_encoder/keymaps/myq11/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/myq11/keymap.c
@@ -44,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MAC_FN] = LAYOUT_91_ansi(
         RM_TOGG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  RM_TOGG,
-        _______,  _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,            _______,
+        _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,            _______,
         _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_PGUP,  LGUI(KC_LEFT), KC_UP, LGUI(KC_RGHT), _______,  _______,  _______,  _______,  _______,
         _______,  _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,              _______,            _______,
-        _______,  _______,            _______,  _______,  _______,  _______,   _______,  KC_NO,    _______,  _______,  _______,  _______,              _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_NO,    _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_91_ansi(
@@ -55,31 +55,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            DF(WIN_BASE),
         _______,  KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             DF(WIN_BASE_JP),
-        _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),         SPC_SYM,                       ENT_CTL,            JP_ZKHK,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),         SPC_SYM,                       ENT_CTL,            KC_F13,   MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_BASE_JP] = LAYOUT_91_ansi(
         KC_MUTE,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_NO,    KC_NO,    KC_MUTE,
         _______,  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_DEL,             DF(MAC_BASE),
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            DF(WIN_BASE),
         _______,  KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             DF(WIN_BASE_JP),
-        _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),        SPC_SYM,                        ENT_CTL,            JP_ZKHK,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  MO(WIN_FN),         SPC_SYM,                       ENT_CTL,            JP_ZKHK,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_91_ansi(
         RM_TOGG,  _______,  KC_BRID,  KC_BRIU,  LGUI(KC_TAB), LGUI(KC_E), RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RM_TOGG,
         _______,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,        KC_F4,      KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     _______,            _______,
         _______,  KC_CAPS,  KC_NO,    KC_NO,    KC_NO,        KC_NO,      KC_NO,     KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   _______,  _______,    _______,  _______,            _______,
         _______,  _______,  KC_NO,    KC_NO,    KC_NO,        KC_NO,      KC_NO,     KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,              _______,            _______,
-        _______,  _______,            _______,  _______,      _______,    _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
+        _______,  _______,  _______,  _______,  _______,      _______,    _______,   _______,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,                _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
     [NUM_SYM] = LAYOUT_91_ansi(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
-        _______,  KC_TILD,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,   _______,            _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_PLUS,  KC_MINS,  KC_EQL,  _______,  _______,    _______,  _______,            _______,
-        _______,  _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,   KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,              KC_PIPE,            _______,
-        _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,
+        _______,  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            _______,
+        _______,  _______,  KC_TILD,  _______,  _______,  _______,  KC_CIRC,   _______,  KC_PLUS,  KC_LBRC,  KC_RBRC,  _______,  _______,    _______,  _______,            _______,
+        _______,  _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PERC,  KC_DLR,    KC_AMPR,  KC_EQL,   KC_LPRN,  KC_RPRN,  KC_UNDS,  _______,              KC_PIPE,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   KC_ASTR,  KC_MINS,  KC_LCBR,  KC_RCBR,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
 };
